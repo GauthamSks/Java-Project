650c22e6a27c5a8d9050b10bf110c97075f21944
Gautham Santhosh <gautham18sks@gmail.com>
2019-09-12 13:00:03 +0530

    Basic Layout of SignIn page is Done

--- Login.java
+++ Login.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import javax.swing.*;
 public class Login{
     public static void main(String[] args) {
@@ -15,5 +17,32 @@ public class Login{
         tab.setBounds(0,40,390,490);  
         J.add(tab);
         J.setVisible(true);
+
+        //SignIn Panel Design
+
+        Sin.setLayout(null);
+        String[] D = {"Student","Faculty"};
+        JComboBox DD = new JComboBox<>(D);
+        DD.setBounds(135,50,100,20);
+        Sin.add(DD);
+        JLabel UID = new JLabel("User Name");
+        UID.setBounds(10,100,100,10);
+        Sin.add(UID);
+        JTextField UIDIn = new JTextField();
+        UIDIn.setBounds(10,120,250,20);
+        Sin.add(UIDIn);
+        JLabel p = new JLabel("Password");
+        p.setBounds(10,155,100,10);
+        Sin.add(p);
+        JPasswordField pf = new JPasswordField();
+        pf.setBounds(10,172,250,20);
+        Sin.add(pf);
+        JButton Lin = new JButton("Sign In");
+        Lin.setBounds(10,205,100,50);
+        Color o = new Color(255,133,26);
+        Lin.setBackground(o);
+        Sin.add(Lin);
+
+        //SignUp Panel Design
     }
 }

cf8bbfb18634f3a416669736a2338879d779042b
Gautham Santhosh <gautham18sks@gmail.com>
2019-09-15 14:34:20 +0530

    Design Completed

--- Login.java
+++ Login.java
@@ -1,17 +1,27 @@
 import java.awt.Color;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.Image;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Login{
     public Login(){
 
+
+        ImagePanel panel1 = new ImagePanel(new ImageIcon("img1*.jpg").getImage());
+        ImagePanel panel2 = new ImagePanel(new ImageIcon("img1*.jpg").getImage());
         JFrame J = new JFrame();
-        Color o1 = new Color(232, 91, 9);
-        Color o2 = new Color(255,133,26);
-        Color o3 = new Color(80, 86, 87);
+        Color o1 = new Color(255, 51, 51);
         J.setSize(390,530);
         J.setLayout(null);
         J.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         J.setLocationRelativeTo(null);
+        J.setContentPane(new JLabel(new ImageIcon("BG*.jpg")));
         J.setTitle("Login");
         JPanel Sin = new JPanel(); //Sign in Panel
         JPanel Sup = new JPanel(); //Sign up Panel
@@ -24,13 +34,10 @@ public class Login{
         J.add(tab);
         J.getContentPane().setBackground(o1);
         J.setVisible(true);
-        
 
         //SignIn Panel Design
         
-        
         Sin.setLayout(null);
-        Sin.setBackground(o3);
         String[] D = {"Student","Faculty"};
         JComboBox DD = new JComboBox<>(D);
         DD.setBounds(135,50,100,20);
@@ -49,11 +56,63 @@ public class Login{
         Sin.add(pf);
         JButton Lin = new JButton("Sign In");
         Lin.setBounds(10,205,100,50);
-        Lin.setBackground(Color.BLUE);
+        Lin.setBackground(Color.GREEN);
         Sin.add(Lin);
-        tab.setBackground(o2);
-    }
+        tab.setBackground(Color.RED);
+        Sin.add(panel1);
+
+        // SignUp panel
+
+        Sup.setLayout(null);
+        JComboBox DD1 = new JComboBox<>(D);
+        DD1.setBounds(135,50,100,20);
+        Sup.add(DD1);
+        JLabel UID1 = new JLabel("User Name");
+        UID1.setBounds(10,100,100,10);
+        Sup.add(UID1);
+        JTextField UIDIn1 = new JTextField();
+        UIDIn1.setBounds(10,120,250,20);
+        Sup.add(UIDIn1);
+        JLabel p1 = new JLabel("Password");
+        p1.setBounds(10,155,100,10);
+        Sup.add(p1);
+        JPasswordField pf1 = new JPasswordField();
+        pf1.setBounds(10,172,250,20);
+        Sup.add(pf1);
+        JButton Lin1 = new JButton("Sign Up");
+        Lin1.setBounds(10,205,100,50);
+        Lin1.setBackground(Color.GREEN);
+        Sup.add(Lin1);
+        tab.setBackground(Color.RED);
+       
+        Sup.add(panel2);
+    } 
     public static void main(String[] args) {
         Login l = new Login();
+        
     }
 }
+
+
+class ImagePanel extends JPanel {
+
+    private Image img;
+    public ImagePanel(String img) {
+      this(new ImageIcon(img).getImage());
+    }
+  
+    public ImagePanel(Image img) {
+      this.img = img;
+      Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+      setPreferredSize(size);
+      setMinimumSize(size);
+      setMaximumSize(size);
+      setSize(size);
+      setLayout(null);
+    }
+  
+    public void paintComponent(Graphics g) {
+      g.drawImage(img, 0, 0, null);
+    }
+  
+  }

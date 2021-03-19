52e11b4a40ecf0dbc8657cdea6a455f845a7a10a
GauthamSks <gautham18sks@gmail.com>
2021-03-19 17:30:21 +0530

    sql dump file added

---
+++ DB.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `Students` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Students`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Students
+-- ------------------------------------------------------
+-- Server version	8.0.19-0ubuntu0.19.10.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `Name` varchar(100) DEFAULT NULL,
+  `ID` varchar(100) DEFAULT NULL,
+  `Subject` varchar(100) DEFAULT NULL,
+  `P1` int DEFAULT NULL,
+  `P2` int DEFAULT NULL,
+  `P3` int DEFAULT NULL,
+  `Grade` varchar(20) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `Field` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('Gautham','U4CSE18117','Maths',45,46,100,NULL,'123','S'),('Gautham','U4CSE18117','DS',47,44,90,NULL,'123','S'),('Gautham','U4CSE18117','Java',42,40,85,NULL,'123','S'),('Usha','TSE1',NULL,0,0,0,NULL,'134','F'),('Eric','U4CSE18116','Maths',20,25,70,NULL,'117','S'),('Eric','U4CSE18116','DS',29,28,75,NULL,'117','S'),('Eric','U4CSE18116','Java',30,28,78,NULL,'117','S'),('Alekhya','U4CSE18104','Maths',35,42,95,NULL,'104','S'),('Alekhya','U4CSE18104','DS',40,41,92,NULL,'104','S'),('Alekhya','U4CSE18104','Java',45,43,91,NULL,'104','S'),('Arjun','U4CSE18108','Maths',35,-1,-1,NULL,'156','S'),('Arjun','U4CSE18108','DS',-1,-1,-1,NULL,'156','S'),('Arjun','U4CSE18108','Java',-1,-1,-1,NULL,'156','S');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-19 16:59:38
